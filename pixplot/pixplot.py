5d8f61a0f4cb322d9ee3d8e1777c455bb95746c4
Matthew Bollinger <32176691+mlybollinger@users.noreply.github.com>
2022-07-21 13:44:28 -0500

    Update pixplot.py

--- pixplot/pixplot.py
+++ pixplot/pixplot.py
@@ -608,7 +608,7 @@ def get_clip_vectors(**kwargs):
         im = preprocess_input( img_to_array( i.original.resize((299,299)) ) )
         image_input = torch.tensor(im).to(device)
         with torch.no_grad():
-          features = torch.squeeze(model.encode_image(torch.unsqueeze(image_input, 0))
+          features = torch.squeeze(model.encode_image(torch.unsqueeze(image_input, 0)))
         vec = features.cpu().detach().numpy()
         np.save(vector_path, vec)
       vecs.append(vec)
